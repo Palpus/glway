8b2edac14588d0911b38d2c8b4ebb1e79380a3f3
unbsold <37555344+unbsold@users.noreply.github.com>
2018-03-23 22:22:44 +0300

    Update footer.php

--- wp-content/themes/glway/footer.php
+++ wp-content/themes/glway/footer.php
@@ -5,6 +5,16 @@
 			<div class="col-md-12 text-center">
 				glway.com &copy 2018
 			</div>
+			<?php
+			$agreement = ( function_exists( 'fw_get_db_settings_option' ) ) ? fw_get_db_settings_option('agreement') : '';
+			if( !empty( $agreement ) ) :
+				?>
+				<a href="#myModal" class="btn btn-primary" data-toggle="modal">Пользовательское соглашение</a>
+				<!-- HTML-код модального окна-->
+				<div id="myModal" class="modal fade">
+					<?php echo $agreement ?>
+				</div>
+			<?php endif ?>
 		</div>
 	</div>
 </footer>
