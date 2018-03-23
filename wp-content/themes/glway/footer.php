772c99ab53b49554cf3222f77a61a4581322e23f
unbsold <37555344+unbsold@users.noreply.github.com>
2018-03-23 22:43:04 +0300

    Update footer.php

--- wp-content/themes/glway/footer.php
+++ wp-content/themes/glway/footer.php
@@ -3,29 +3,29 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12 text-center">
-				glway.com &copy 2018
+				glway.com &copy 2018<br>
 				<?php
 				$agreement = ( function_exists( 'fw_get_db_settings_option' ) ) ? fw_get_db_settings_option('agreement') : '';
 				if( !empty( $agreement ) ) :
 					?>
 					<a href="#myModal" data-toggle="modal">Пользовательское соглашение</a>
-					<!-- HTML-код модального окна-->
-					<div id="myModal" class="modal fade">
-					<div class="modal-dialog">
-					    <div class="modal-content">
-					      <!-- Заголовок модального окна -->
-					      <div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-						<h4 class="modal-title">Пользовательское соглашение</h4>
-					      </div>
-					      <!-- Основное содержимое модального окна -->
-					      <div class="modal-body">
-						<?php echo $agreement ?>
-					       </div>
-					    </div>
-					  </div>
-					</div>
 				<?php endif ?>
+			</div>
+			<!-- HTML-код модального окна-->
+			<div id="myModal" class="modal fade">
+			<div class="modal-dialog">
+			    <div class="modal-content">
+			      <!-- Заголовок модального окна -->
+			      <div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+				<h4 class="modal-title">Пользовательское соглашение</h4>
+			      </div>
+			      <!-- Основное содержимое модального окна -->
+			      <div class="modal-body">
+				<?php echo $agreement ?>
+			       </div>
+			    </div>
+			  </div>
 			</div>
 
 		</div>
