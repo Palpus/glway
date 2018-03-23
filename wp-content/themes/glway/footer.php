23d3e7505636e82f6c886149f51834b72c9ee06b
unbsold <37555344+unbsold@users.noreply.github.com>
2018-03-23 22:27:45 +0300

    Update footer.php

--- wp-content/themes/glway/footer.php
+++ wp-content/themes/glway/footer.php
@@ -12,7 +12,19 @@
 				<a href="#myModal" class="btn btn-primary" data-toggle="modal">Пользовательское соглашение</a>
 				<!-- HTML-код модального окна-->
 				<div id="myModal" class="modal fade">
+				<div class="modal-dialog">
+				    <div class="modal-content">
+				      <!-- Заголовок модального окна -->
+				      <div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+					<h4 class="modal-title">Пользовательское соглашение</h4>
+				      </div>
+				      <!-- Основное содержимое модального окна -->
+				      <div class="modal-body">
 					<?php echo $agreement ?>
+				       </div>
+				    </div>
+				  </div>
 				</div>
 			<?php endif ?>
 		</div>
