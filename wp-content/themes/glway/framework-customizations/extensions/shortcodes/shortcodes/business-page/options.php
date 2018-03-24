38954e5bd65a01ed9ab7bf4bfc407626f299254f
unbsold <37555344+unbsold@users.noreply.github.com>
2018-03-24 16:46:25 +0300

    Update options.php

--- wp-content/themes/glway/framework-customizations/extensions/shortcodes/shortcodes/business-page/options.php
+++ wp-content/themes/glway/framework-customizations/extensions/shortcodes/shortcodes/business-page/options.php
@@ -21,7 +21,7 @@ $options = array(
 				'label'  => __( 'Заголовок события', 'fw' )
 			),
 			'tab_content' => array(
-				'type'  => 'wp-text',
+				'type'  => 'wp-editor',
 				'desc'  => __( 'Описание бизнеса', 'fw' ),
 				'label' => __( 'Текст бизнеса', 'fw')
 			),
